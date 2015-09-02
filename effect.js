1595d6ff5159fe1b38e4cf333139fae658347219
John Jannetty <jjannetty@thelevelup.com>
2015-09-01 21:34:09 -0400

    optimize some js

--- effect.js
+++ effect.js
@@ -11,11 +11,12 @@ $.fn.lightSource = function(options) {
       offsetY: 0,
       lightWidth: 700,
       lightHeight: 700,
-      lightOpacity: 0.02,
-      lightBlur: 40
+      lightOpacity: 0.08,
+      lightBlur: 40,
+      lightColor: '#FFFFCC',
     }, options);
 
-    var shadowValue = 'drop-shadow('+ settings.offsetX + 'px ' + settings.offsetY + 'px ' + settings.blur + 'px rgba(0,0,0,'+ settings.opacity +'))';
+    var shadowValue = ['drop-shadow(', settings.offsetX, 'px ', settings.offsetY, 'px ' , settings.blur, 'px rgba(0,0,0,', settings.opacity,'))'].join('');
 
     settings.viewport.css({
       position: 'relative',
@@ -33,9 +34,10 @@ $.fn.lightSource = function(options) {
       marginLeft: -(settings.lightWidth / 2),
       marginTop: -(settings.lightHeight / 2),
       opacity: settings.lightOpacity,
-      background: 'white',
-      filter: 'blur(' + settings.lightBlur + 'px)',
-      webkitFilter: 'blur(' + settings.lightBlur + 'px)',
+      background: settings.lightColor,
+      filter: ['blur(', settings.lightBlur, 'px)'].join(''),
+      webkitFilter: ['blur(', settings.lightBlur, 'px)'].join(''),
+      mozFilter: ['blur(', settings.lightBlur, 'px)'].join(''),
       width: settings.lightWidth,
       height: settings.lightHeight,
       borderRadius: (settings.viewport.width() / 2) + 'px',
@@ -45,6 +47,7 @@ $.fn.lightSource = function(options) {
     settings.target.css({
       filter: shadowValue,
       webkitFilter: shadowValue,
+      mozFilter: shadowValue,
       position: 'relative',
       zIndex: 10
     });
@@ -60,7 +63,7 @@ $.fn.lightSource = function(options) {
       var yDistance = distanceFromCenterY > 0 ? distanceFromCenterY : -distanceFromCenterY;
       var xyAvg = (xDistance + yDistance) / 4;
       var distanceBlur = settings.distanceBlur == true ? settings.blur + (xyAvg/settings.distanceBlurAmount) : settings.blur;
-      var shadowValue = 'drop-shadow('+ (-distanceFromCenterX) + 'px ' + (-distanceFromCenterY) + 'px ' + distanceBlur + 'px rgba(0,0,0,'+ settings.opacity +'))';
+      var shadowValue = ['drop-shadow(', (-distanceFromCenterX), 'px ', (-distanceFromCenterY), 'px ', distanceBlur, 'px rgba(0,0,0,', settings.opacity,'))'].join('');
       var lightOffsetX = -(settings.lightWidth / 2) - distanceFromCenterX;
       var lightOffsetY = -(settings.lightHeight / 2) - distanceFromCenterY;
 
